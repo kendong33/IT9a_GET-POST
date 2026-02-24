309029dd8b850d78c09f5c932efecdd98095db35
kendong33 <kendongvrgs@gmail.com>
2026-02-24 09:54:47 +0800

    Add files via upload

---
+++ array/mathstring.php
@@ -0,0 +1,49 @@
+<?php
+
+echo "<h2>Math Functions</h2>";
+
+$number = -10;
+echo "abs ($number) = " . abs ($number) . "<br>";
+
+$number = 4.6;
+echo "round ($number) = " . round ($number) . "<br>";
+
+$number = 4.2;
+echo "ceil ($number) = " . ceil ($number) . "<br>";
+
+$number = 4.5;
+echo "floor ($number) = " . floor ($number) . "<br>";
+
+$base = 2;
+$exp = 3;
+echo "pow($base, $exp) = " . pow($base, $exp) . "<br>";
+
+$number = 16;
+echo "sqrt($number) = " . sqrt($number) . "<br>";
+
+echo "max(1, 5, 3) = " . max(1, 5, 3) . "<br>";
+echo "min(1, 5, 3) = " . min(1, 5, 3) . "<br>";
+
+echo "rand(1, 100) = " . rand(1, 100) . "<br>";
+
+echo "<h2> String Function</h2>";
+
+$str = "Hello World";
+echo "strlen('$str') = " . strlen($str) . "<br>";
+
+echo "strtoupper('$str') = " . strtoupper($str) . "<br>";
+
+echo "strtolower('$str') = " . strtolower($str) . "<br>";
+
+$str3 = "apple,banana,orange";
+$fruits = explode(",", $str3);
+echo "explode(',', '$str3') = ";
+print_r($fruits);
+echo "<br>";
+
+$joined = implode(" - ", $fruits);
+echo "implode(' - ', fruits) = $joined<br>"
+
+
+echo "strrev('$str') = " . strrev($str) . "<br>";
+?>
